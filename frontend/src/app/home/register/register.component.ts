b8f7c9f61c2f26a777e4e2501c9cb1ede4493985
DANIELPACHECORINCON <danielpacherincon@gmail.com>
2022-03-04 10:55:09 -0500

    se agrego el login logout

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationinSecons: number = 2000;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _userServisce: UserService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.registerData = {};
   }
 
+  registerUser() {
+    if (
+      !this.registerData.name ||
+      !this.registerData.email ||
+      !this.registerData.password
+    ) {
+      this.message = 'Incomplete data';
+      this.openSnackBarError();
+    } else {
+      this._userServisce.registerUser(this.registerData).subscribe({
+        next: (v) => {
+          localStorage.setItem('token', v.token);
+          this._router.navigate(['/saveTask']);
+          this.message = 'Succesfull user registration';
+          this.openSnackBarSuccesfull();
+        },
+        error: (e) => {
+          this.message = e.error.message;
+          this.openSnackBarError();
+        },
+      });
+    }
+  }
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationinSecons,
+      panelClass: ['styleSnackBarSuccesfull'],
+    });
+  }
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationinSecons,
+      panelClass: ['styleSnackBarError'],
+    });
+  }
+
+  ngOnInit(): void {}
 }
